5117f3f87e2cffcf01d19c069e8fa424ffdd073f
vitor-nery11 <vitornerysantos24@gmail.com>
2026-07-17 19:44:37 -0300

    feat: implementando HTTPException na minha api

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -26,6 +26,12 @@ def listar_livros():
 
 @app.get('/livros/{id}')
 def buscar_livro(id:int):
+    if id < 0 and id >= len(livros):
+        raise HTTPException(
+            status_code=404,
+            detail='Livro não encontrado'
+        )
+
     return livros[id]
 
 
@@ -41,6 +47,11 @@ def criar_livro(livro:Livro):
 # ROTAS PUT
 @app.put('/livros/{id}')
 def atualizar_livro(id:int, livro:Livro):
+    if id < 0 and id >= len(livros):
+        raise HTTPException(
+            status_code=404,
+            detail='Livro não encontrado'
+        )
     livros[id]= livro
     return {
         'mensagem': 'livro atualizado com sucesso'
@@ -51,6 +62,11 @@ def atualizar_livro(id:int, livro:Livro):
 # ROTA DELETE 
 @app.delete('/livros/{id}')
 def deletar_livro(id: int):
+    if id < 0 and id >= len(livros):
+        raise HTTPException(
+            status_code=404,
+            detail='Livro não encontrado'
+        )
     livros.pop(id)
     return {
         'mensagem': 'livro removido'
